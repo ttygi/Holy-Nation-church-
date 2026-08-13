626914de04bfe966cde83faa92bc019bc3f37da7
ttygi <bishopchihana897@gmail.com>
2026-08-13 13:34:30 +0200

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = "https://hxonnzbdkmbitwtpbyic.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_iMpl2OAbQ2PwmNGXdLKNeA_GPlHuxYs";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
